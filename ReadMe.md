14ac754fab162c3bdfa103b1ae8af566ff5280ca
Gundo Sifhufhi <gundo.sifhufhi@vaimo.com>
2023-10-13 22:48:03 +0200

    Shell Shill, Shill Shells

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+Instructions for running the code
+=================================
+
+1. Run the script with the following command in the terminal (make sure you are in the same directory as the script):
+chmod +x sagecall.sh
+./sagecall.sh
+
+
+##TO Call the script from anywhere in the system ( run the following command in the terminal):
+- sudo cp sagecall.sh /usr/local/bin
+##Then run the following commands:
+- sudo chmod +x /usr/local/bin/sagecall.sh
+- sagecall.sh
---
+++ gptcli.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+OPENAI_API_TOKEN="YOUR API TOKEN HERE"
+# Check if the OPENAI_API_TOKEN environment variable is set
+if [ -z "$OPENAI_API_TOKEN" ]; then
+    echo "Please set your OpenAI API token using the environment variable OPENAI_API_TOKEN."
+    exit 1
+fi
+
+# Define the prompt from the command line argument
+PROMPT="$1"
+
+# Check if the prompt argument is provided
+if [ -z "$PROMPT" ]; then
+    echo "Please provide a prompt as the argument."
+    exit 1
+fi
+
+# Use curl to make the API call
+curl --location 'https://api.openai.com/v1/completions' \
+--header 'Content-Type: application/json' \
+--header "Authorization: Bearer $OPENAI_API_TOKEN" \
+--data '{
+  "model": "davinci",
+  "prompt": "'"$PROMPT"'",
+  "max_tokens": 100,
+  "temperature": 0,
+  "top_p": 1,
+  "n": 1,
+  "stream": false,
+  "logprobs": null,
+  "stop": "\n"
+}' | jq -r '.choices[0].text'
